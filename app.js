754ebe0a9dc82faeea73d8aa56e22f0dccc691ca
Madhushri Karekar <madhushrikarekar16@gmail.com>
2023-09-26 20:12:47 +0530

    add files

---
+++ app.js
@@ -0,0 +1,20 @@
+// let url = "https://world.openfoodfacts.org/api/v0/product/737628064502.json";
+// let btn = document.querySelector("button");
+
+// btn.addEventListener("click", () => {
+//   getData();
+// });
+
+// async function getData() {
+//   let data = await fetchData();
+//   console.log(data);
+// }
+
+// async function fetchData() {
+//   try {
+//     let res = await axios.get(url);
+//     return res.data.product;
+//   } catch (error) {
+//     console.log("error -", error);
+//   }
+// }
